be94d7b17e764d6be8a7a07a7e296dd3f4476ed8
Kolchin13D <kolchin13d@gmail.com>
2024-09-11 11:28:23 +0300

    testing

--- app/src/main/java/com/example/findwork/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/findwork/ui/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -72,6 +73,14 @@ class SearchFragment : Fragment() {
 
         val offerAdapter = OfferAdapter(offerList)
         recyclerViewOffer.adapter = offerAdapter
+
+        binding.moreVacanciesBtn.setOnClickListener{
+            Toast.makeText(
+                context,
+                "You select more vacancies",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 
         return root
     }
